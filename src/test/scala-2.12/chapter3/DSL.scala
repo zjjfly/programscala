98ad024678edc6b1f300dc468db5783145ae7f90
zjjfly <zjjblue@gmail.com>
2017-01-20 23:53:03 +0800

    使用Matchers

--- src/test/scala-2.12/chapter3/DSL.scala
+++ src/test/scala-2.12/chapter3/DSL.scala
@@ -1,15 +1,15 @@
 package chapter3
 
-import org.scalatest.{FunSpec,MustMatchers}
+import org.scalatest.{FunSpec, Matchers}
 
 /**
   * Created by zjjfly on 2016/11/4.
   */
-class DSL extends FunSpec with MustMatchers{
-  describe("nerd finder"){
-    it ("identify nerds from a List") {
-      val actors=List("Rick Moranis","James Dean","Woody Allen")
-      actors mustEqual  List("Rick Moranis","James Dean","Woody Allen")
+class DSL extends FunSpec with Matchers {
+  describe("nerd finder") {
+    it("identify nerds from a List") {
+      val actors = List("Rick Moranis", "James Dean", "Woody Allen")
+      actors shouldEqual List("Rick Moranis", "James Dean", "Woody Allen")
     }
   }
 }
