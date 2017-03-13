e6ec8893a7316fbe17586b705ed808c141a75b58
zjjfly <zjjblue@gmail.com>
2017-03-13 17:45:13 +0800

    使用隐式参数

--- src/main/scala-2.12/chapter5/ImplicitErasure.scala
+++ src/main/scala-2.12/chapter5/ImplicitErasure.scala
@@ -1,5 +1,7 @@
 package chapter5
 
+import scala.annotation.implicitNotFound
+
 /**
   * Created by zjjfly on 2017/1/29.
   */
@@ -9,11 +11,22 @@ object M{
 //  def m(seq: Seq[Int]):Unit= println(s"Seq[Int]:$seq")
 //  def m(seq: Seq[String]):Unit= println(s"Seq[String]:$seq")
 
-  implicit object IntMarker
-  implicit object StringMarker
-
   //使用隐式参数，可以解决上面的编译问题
   //不使用Int，String这样的类型的隐式参数的原因是，这些类型更有可能会在多处定义器对应的隐式对象。如果这些隐式定义被导入相同的作用域，会导致程序bug或者编译错误
   def m(seq: Seq[Int])(implicit i:IntMarker.type):Unit= println(s"Seq[Int]:$seq")
   def m(seq: Seq[String])(implicit i:StringMarker.type):Unit= println(s"Seq[String]:$seq")
+}
+//另一个不是用常用类型作为隐式参数的原因是，可以使用@implicitNotFound添加用户友好的错误消息
+//@implicitNotFound只能用于需要作为隐式参数的类型上，而不是用于含有隐式参数的方法。
+@implicitNotFound(msg = "can't fidnd int marker!")
+object IntMarker
+@implicitNotFound(msg = "can't fidnd string marker!")
+object StringMarker
+
+object Test{
+  implicit val obj=IntMarker
+  def main(args: Array[String]): Unit = {
+    M.m(Seq(1,2))
+    M.m(Seq("1","2"))
+  }
 }
