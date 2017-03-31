537b482ed4bf538069f3ddebb7ac93b31634ad11
zjjfly <zjjblue@gmail.com>
2017-03-31 16:57:03 +0800

    +Set,Sequence +Collection FlatMapping、Mapping、Folding、Filtering

---
+++ src/main/scala-2.12/chapter6/Mapping.scala
@@ -0,0 +1,14 @@
+package chapter6
+
+/**
+  * Created by zjjfly on 2017/3/27.
+  */
+object Mapping extends App{
+  val intToString = (i:Int) => s"N=$i"
+  val flist = Combinator.map(intToString) _
+  val list = flist(List(1,2,3,4))
+  assert(List("N=1", "N=2", "N=3", "N=4")==list)
+}
+object Combinator {
+  def map[A,B](f: (A) ⇒ B)(list: List[A]): List[B] = list map f
+}
---
+++ src/main/scala-2.12/chapter6/Maps.scala
@@ -0,0 +1,17 @@
+package chapter6
+
+/**
+  * Created by zjjfly on 2017/3/26.
+  */
+object Maps extends App {
+  val nums1 = Map("1" -> 1, "2" -> 2)
+  val nums2 = nums1.map(kv => kv._1 + kv._2)
+  val nums3 = nums1.map {
+    case (k, v) => k + v //使用partial function的好处是可以使用模式匹配
+  }
+  assert(nums2==nums3)
+  val nums4 = nums1 + ("3" -> 3)
+  assert(nums4 == Map("1" -> 1, "2" -> 2, "3" -> 3))
+  val nums5 = nums1 - ("1")
+  assert(nums5 == Map("2" -> 2))
+}
